b50e4ab58fb99243a25c9a1357f4f2adfca538aa
kekuBT <69989635+kekuBT@users.noreply.github.com>
2022-05-17 21:56:55 +0300

    Navigation is responsive and other edits

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-FormValidation.formValidation(
-    document.getElementById('registrationForm'),
-    {
-        fields: {
-            userName: {
-                validators: {
-                    notEmpty: {
-                        message: 'The username is required',
-                    },
-                    stringLength: {
-                        message: 'The name must be more than 6 characters long',
-                        min: 6,
-                    },
-                },
-            },
-        },
-    },
-);
+// --------- Navigation responsive ----------------
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navMenu = document.querySelectorAll('.nav-menu li');
+
+    // Set nav active status with boolean
+    let isActive = false;
+    console.log("nav-active : " + isActive)
+
+
+    burger.addEventListener("click", () => {
+
+        //Toggle nav
+        nav.classList.toggle("nav-active");
+
+        // Toggle nav active status to true/false
+        isActive = !isActive;
+        console.log("nav-active : " + isActive)
+
+        //Animate link
+        navMenu.forEach((link, index) => {
+            if (isActive) {
+                link.style.animation = `navMenuFadeIn 0.4s ease forwards ${index / 7 + 0.2}s`;
+                console.log("nav li: " + index + " in");
+            } else {
+                // link.style.animation = "";
+                link.style.animation = `navMenuFadeOut 0.2s ease forwards 0s`;
+                // link.style.animation = "";
+                console.log("nav li: " + index + " out");
+            }
+            console.log(link.style.animation);
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+navSlide();
+
+// -------- Password hide and show ------------
 
 var state = false;
 function toggle() {
